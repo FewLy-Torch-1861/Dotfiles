89aa284fc4f78592c2d25c4b0c1ec0559dc99cfc
FewLy-Torch-1861 <furytorch.1861@gmail.com>
2025-06-14 08:14:54 +0700

    update

--- update.sh
+++ update.sh
@@ -2,8 +2,18 @@
 
 set -euo pipefail
 
+echo "[*] Updating pacman and aur..."
+
 yay -Syyu
+
+echo "[*] Updating flatpak..."
+
 flatpak update
+
+echo "[*] Commiting dotfiles change..."
+
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME commit -a -m "update"
+
+echo "[*] Doned exiting..."
 
 exit 0
