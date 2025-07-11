2ef55662c5dcff3c2d2948b89d51fe23522cd16d
FewLy-Torch-1861 <furytorch.1861@gmail.com>
2025-07-11 12:15:50 +0700

    fogot to add night-light.sh

---
+++ .config/hypr/scripts/rofi/night-light.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+rofi_command="rofi -i -dmenu -no-show-icons -config ${HOME}/.config/rofi/config-toggle.rasi"
+toggle=(
+  " Enabled"
+  " Disabled"
+)
+
+selection=$(for i in "${toggle[@]}"; do echo "$i"; done | ${rofi_command} -p "󱩌 Night light")
+actual_selection=$(echo "$selection" | sed 's/^[^ ]* //')
+
+case "$actual_selection" in
+Enabled)
+  hyprsunset -t 5000 &
+
+  notify-send "Hyprland" "Enabled night light"
+  ;;
+Disabled)
+  pkill hyprsunset
+
+  notify-send "Hyprland" "Disabled night light"
+  ;;
+*)
+  echo "No match for: $actual_selection" >&2
+  exit 1
+  ;;
+esac
