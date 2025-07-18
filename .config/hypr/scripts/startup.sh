0341b7bd6494f0acfea213ce89a54ee46c34d22b
FewLy-Torch-1861 <furytorch.1861@gmail.com>
2025-07-18 18:33:23 +0700

    update: 2025-07-18_18-33-23

--- .config/hypr/scripts/startup.sh
+++ .config/hypr/scripts/startup.sh
@@ -21,10 +21,9 @@ swww-daemon & # WALLPAPER!
 swaync &      # The Notification daemon
 waybar &      # The Status bar
 
-discord --ozone-platform-hint=auto &                                                # DISCORD!!
 $BROWSER || notify-send -a "Hyprland" "startup.sh" "Startup Browser is not set\!" & # I'm just to lazy to press SUPER + B
 
-sleep 10
+sleep 8
 
 hyprctl -q dispatch workspace 2 # Go to my most used workspace
 
