a7ad10031cc3c5cf5583e81e431aaa31983be690
FewLy-Torch-1861 <furytorch.1861@gmail.com>
2025-09-15 00:31:06 +0700

    make fish config look better

--- terminal/.config/fish/config.fish
+++ terminal/.config/fish/config.fish
@@ -1,90 +1,26 @@
-if status is-interactive # Commands to run in interactive sessions can go here
+if status is-interactive
+  ## ── Greeting ───────────────────────────
   set fish_greeting
 
-  # Init Zoxide
+  ## ── Prompt ─────────────────────────────
   zoxide init fish | source
+  starship init fish | source
 
-  # Init Starship
-  if test -d "/data/data/com.termux/files/home"
-    # Termux
-    source (/data/data/com.termux/files/usr/bin/starship init fish --print-full-init | psub)
-  else
-    # Linux
-    source (/usr/bin/starship init fish --print-full-init | psub)
-  end
-
-  # Shortcut
+  ## ── Aliases & Abbr ─────────────────────
+  # Core shortcuts
   alias reload 'exec fish'
-  alias cls='clear'
-  alias ff='fastfetch'
-  alias vi=nvim
-  alias y=yazi
-  alias cd=z
+  alias cls 'clear'
+  alias ff 'fastfetch'
+  alias vi 'nvim'
+  alias y 'yazi'
+  alias cg 'cargo'
+  alias py 'python'
+  alias cd 'z'
+  alias grep 'grep --color=auto'
 
-  # Misc
-  function md # create dir
-    mkdir -pv "$argv[1]"
-    cd "$argv[1]"
-  end
-
-  function tf # touch file with mkdir -p
-    set file $argv[1]
-    if test -z "$file"
-      echo "Usage: tf <path/to/file>"
-      return 1
-    end
-
-    set dir (dirname "$file")
-    mkdir -pv "$dir"
-    touch "$file"
-    echo "touch: created file '$file'"
-    cd "$dir"
-  end
-
-  function cfp # copy file path
-    readlink -f "$argv[1]" | wl-copy
-  end
-
-
-  if not test -d "/data/data/com.termux/files/home"
-    alias cdp='pwd | wl-copy' # copy dir path
-  end
-  alias grep='grep --color=auto'
-
-  # Search
-  abbr --add f 'fzf'
-  abbr --add ffind 'fd --type f'
-  abbr --add rg 'rg --color=always'
-
-  # Archive shortcuts
-  abbr --add untar 'tar -xvf'
-  abbr --add ungz 'gunzip'
-  abbr --add unbz2 'bunzip2'
-  abbr --add unzipr 'unzip -d'
-
-  # Pastel
-  if not test -d "/data/data/com.termux/files/home"
-    alias p='pastel'
-    alias pf='pastel format'
-    alias pc='pastel color'
-    alias pg='pastel gradient'
-  end
-
-  # Development
-  alias g='git'
-  alias lg='lazygit'
-  alias cg='cargo'
-  alias py='python'
-  abbr --add pvenv 'python3 -m venv .venv'
-  abbr --add pactv 'source .venv/bin/activate.fish'
-  abbr --add pipi 'pip install -U pip'
-  abbr --add pipr 'pip install -r requirements.txt'
-
-  function gc # TODO: make also work when specifying dir
-    git clone $argv[1]
-    set repo (basename (string replace -r '\.git$' '' $argv[1]))
-    cd $repo
-  end
+  # Git
+  alias g 'git'
+  alias lg 'lazygit'
 
   abbr --add gcm 'git commit -m'
   abbr --add ga 'git add'
@@ -99,8 +35,19 @@ if status is-interactive # Commands to run in interactive sessions can go here
   abbr --add gundo 'git reset --soft HEAD~1'
   abbr --add gclean 'git clean -fd'
 
-  # List Family
-  alias ls='eza --icons'
+  # Search & Files
+  abbr --add f 'fzf'
+  abbr --add ffind 'fd --type f'
+  abbr --add rg 'rg --color=always'
+
+  # Archive
+  abbr --add untar 'tar -xvf'
+  abbr --add ungz 'gunzip'
+  abbr --add unbz2 'bunzip2'
+  abbr --add unzipr 'unzip -d'
+
+  # LS family (using eza)
+  alias ls 'eza --icons'
   abbr --add l 'ls -lAh'
   abbr --add ll 'ls -lh'
   abbr --add la 'ls -Ah'
@@ -108,25 +55,66 @@ if status is-interactive # Commands to run in interactive sessions can go here
   abbr --add tea 'ls -AhT --level=2'
 
   # Safety
-  alias rm='gio trash'
+  alias rm 'gio trash'
 
-  # System Maintenance
-  if not test -d "/data/data/com.termux/files/home"
+  ## ── Functions ──────────────────────────
+  function md
+    # mkdir + cd
+    mkdir -pv "$argv[1]"
+    cd "$argv[1]"
+  end
+
+  function tf
+    # touch file with auto create dir ig not exsit
+    set file $argv[1]
+    if test -z "$file"
+      echo "Usage: tf <path/to/file>"
+      return 1
+    end
+    mkdir -pv (dirname "$file")
+    touch "$file"
+    echo "touch: created file '$file'"
+    cd (dirname "$file")
+  end
+
+  function gc
+    # clone repo + cd เข้า dir
+    git clone $argv[1]
+    set repo (basename (string replace -r '\.git$' '' $argv[1]))
+    cd $repo
+  end
+
+  ## ── Env Specific ───────────────────────
+  if set -q TERMUX_VERSION
+    ### ── TERMUX ONLY ─────────────────────
+  else
+    ### ── LINUX ONLY ──────────────────────
+    ## ── Aliases & Abbr ───────────────────
+    alias cdp 'pwd | wl-copy'
+
     abbr --add cleanlog 'sudo journalctl --vacuum-time=7d'
     abbr --add listpkg 'yay -Q | fzf -e --no-preview'
-    abbr --add clnpkg 'yay -Rns $(yay -Qtdq)'
+    abbr --add clnpkg 'yay -Rns (yay -Qtdq)'
+
+    ## ── Functions ────────────────────────
+    function cfp
+      # copy file path
+      readlink -f "$argv[1]" | wl-copy
+    end
   end
 
   clear
 end
 
-# ENVs
-if not test -d "/data/data/com.termux/files/home"
+## ── Global ENVs ──────────────────────────
+set -x EDITOR nvim
+
+fish_add_path $HOME/.local/bin
+
+if not set -q TERMUX_VERSION
   fish_add_path $HOME/.spicetify
+
   set -x FZF_DEFAULT_OPTS "\
   --layout=reverse \
   --border"
 end
-
-fish_add_path $HOME/.local/bin
-set -x EDITOR nvim
