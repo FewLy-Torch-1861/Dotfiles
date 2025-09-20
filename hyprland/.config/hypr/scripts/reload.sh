1916c06b7f8f9f45bea38cc1596c59aacfe353dc
Nutt <furytorch.1861@gmail.com>
2025-09-20 22:19:34 +0700

    add hyprsunset and hypridle config!

--- hyprland/.config/hypr/scripts/reload.sh
+++ hyprland/.config/hypr/scripts/reload.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
-swww kill || true; swww-daemon&
-pkill waybar || true; waybar&
+swww kill || true
+swww-daemon&
+
+pkill waybar || true
+waybar&
+
+pkill hypridle || true
+hypridle&
+
+pkill hyprsunset || true
+sleep 1 && hyprsunset&
+
 makoctl reload&
 hyprctl reload&
