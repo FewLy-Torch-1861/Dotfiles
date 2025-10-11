6de4b0b3d5937f8ba44475445773791ea53e5c2e
ナッティ <furytorch.1861@gmail.com>
2025-10-11 12:38:37 +0700

    auto copy image

--- .config/sway/scripts/screenshot.sh
+++ .config/sway/scripts/screenshot.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 SCREENSHOT_DIR="$HOME/Pictures/Screenshots"
 SCREENSHOT_FILE="Screenshot-$(date +%y-%m-%d-%H-%M-%S).png"
 FULL_PATH="$SCREENSHOT_DIR/$SCREENSHOT_FILE"
@@ -9,20 +8,25 @@ mkdir -p "$SCREENSHOT_DIR"
 case "$1" in
 full)
   grim -t png "$FULL_PATH"
+  cat "$FULL_PATH" | wl-copy --type image/png
   notify-send "Screenshot Taken"
   ;;
 select)
-  grim -t png -g "$(slurp)" "$FULL_PATH"
-  notify-send "Screenshot Taken"
+  if grim -t png -g "$(slurp)" "$FULL_PATH"; then
+    cat "$FULL_PATH" | wl-copy --type image/png
+    notify-send "Screenshot Taken"
+  fi
   ;;
 window)
   GEOMETRY=$(swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?) | "\(.rect.x+.window_rect.x),\(.rect.y+.window_rect.y) \(.window_rect.width)x\(.window_rect.height)"' | slurp)
+
   if [ -n "$GEOMETRY" ]; then
     grim -t png -g "$GEOMETRY" "$FULL_PATH"
+    cat "$FULL_PATH" | wl-copy --type image/png
+    notify-send "Screenshot Taken"
   fi
-  notify-send "Screenshot Taken"
   ;;
--h|--help|*)
+-h | --help | *)
   echo "Usage: $0 <full|select|window>"
   exit 1
   ;;
